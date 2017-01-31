011e3f08b388d69c7f45e82f02d6828365da3efc
Squizzy <crazy_squirrel@yahoo.com>
2017-01-31 02:42:12 +0000

    Tree for the Xcode project

---
+++ Project/NHK-World-XML-to-XMLTV/NHK-World-XML-to-XMLTV/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NHK-World-XML-to-XMLTV
+//
+//  Created by Guillaume on 31/01/2017.
+//  Copyright © 2017 CrazySquirrel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
