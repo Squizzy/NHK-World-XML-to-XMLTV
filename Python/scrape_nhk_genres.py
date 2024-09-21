8bafdec2eea816228069995a9a11b76bfeb81442
Squizzy <crazy_squirrel@yahoo.com>
2024-09-21 20:43:23 +0100

    split into various functions, and modularised for the main app to call get_genres() and return a updated dictionary

--- Python/scrape_nhk_genres.py
+++ Python/scrape_nhk_genres.py
@@ -1,4 +1,5 @@
-""" Python application to convert NHK EPG in JSON into XMLTV standard"""
+""" Python (python 3 only) application to fetch the EPG genres that NHK uses"""
+
 __author__ = "Squizzy"
 __copyright__ = "Copyright 2019-now, Squizzy"
 __credits__ = "The respective websites, and whoever took time to share information\
@@ -14,9 +15,18 @@ import re
 from bs4 import BeautifulSoup
 
 
+# Download the content of pages baseNHKGrensURL/<number> 
+# where <number> is the genre. 
+# If there is a genre, there is a page, it seems.
+# and extract the genre from the title (Thank you NHK!). 
+# Update the genres dictionary if a page existed
+# Saves the list of a file
+
 
 # Location of where the NHK categories could be found (at the time of the creation of this file)
-baseNHKGenresURL = "https://www3.nhk.or.jp/nhkworld/en/shows/category/"
+BASE_URL_FOR_NHK_GENRES = "https://www3.nhk.or.jp/nhkworld/en/shows/category/"
+
+SAVED_GENRES_FILE = "genres.txt"
 
 # Type hint then initiate genres with the None value (used when no genre is provided)
 genres: dict[int|None, str]
@@ -26,34 +36,99 @@ genres = {
     }
 
 
-def parse_html(url):
-    """Fetches content from a URL and parses it with BeautifulSoup."""
+def parse_html(url) -> BeautifulSoup|None:
+    """ Fetches content from a URL and parses it with BeautifulSoup.
+    
+    Args:
+        url (str): the URL to try and fetch from the internet
+
+    Returns:
+        str|None: the html of the requested page, if it exists, otherwise None
+    """
     response = requests.get(url)
     if response.status_code == 200:
         # Parse the HTML content
         soup = BeautifulSoup(response.text, 'html.parser')
         return soup
     else:
-        print(f"Failed to retrieve the webpage: status code {response.status_code}")
+        # print(f"Failed to retrieve the webpage: status code {response.status_code}")
+        print("Not assigned by NHK")
         return None
 
 
-# Download the content of pages baseNHKGrensURL/<number> 
-# where <number> is the genre. 
-# If there is a genre, there is a page, it seems.
-# and extract the genre from the title (Thank you NHK!). 
-# Update the genres dictionary if a page existed
+def indentify_and_acquire_genre(genre) -> str|None:
+    """ For a given genre number, try to retrieve the assigned string value
 
-for genre in range(100):
-    print(genre)
-    soup = parse_html(baseNHKGenresURL + str(genre))
+    Args:
+        genre (int): integer that may have a genre assigned to, by NHK
+
+    Returns:
+        str|None: the genre, in english, of the corresponding genre number, otherwise None
+    """
+    # for genre in range(100):
+    # print the number with leading space(s) if less that 3 digits
+    print(f"{genre: >3d}: ", end="")
+    
+    soup = parse_html(BASE_URL_FOR_NHK_GENRES + str(genre))
+    
     if soup:
-        title = soup.find('title').text
-        value =  str((re.findall('Watch (.*) Videos', title)))
-        value = value[2:][:len(value)-4]
-        genres[int(genre)]= value
+        # print(soup)
+        title = soup.title.text # type: ignore
+        
+        value = (re.findall('Watch (.*) Videos', title))[0]
+        print(value)
+        return value
+    else:
+        # print("Not assigned.")
+        return None
 
-# Save the formatted version of the dictionar into a file, 
-# ready to be copy/pasted into the NHK XMLTV generator app
-with open('genres.txt', 'w') as f:
-    f.write("genres = " + str(genres).replace(", ", ",\n          ").replace("}", "\n}"))
+
+def save_genres() -> None:
+    """ Saves the list of genres to a file.
+    Currently no error checking is performed
+    """
+    # Save the formatted version of the dictionar into a file, 
+    # ready to be copy/pasted into the NHK XMLTV generator app
+        # f.write("genres = " + str(genres).replace(", ", ",\n          ").replace("}", "\n}"))            
+
+    with open(SAVED_GENRES_FILE, 'w') as f:
+        f.write("genres = {\n")
+        for key in genres:
+            f.write(f'\t{str(key)}: "{str(genres[key])}"\n')
+        f.write("}")
+
+
+def get_genres(min:int = 0, max:int = 100) -> dict[int|None, str]:
+    """retrives all the genres in the min-max range given and return a dictionary of the ones found
+
+    Args:
+        min (int, optional): lowest genre number, included. Defaults to 0.
+        max (int, optional): highest genre number, not included. Defaults to 100.
+
+    Returns:
+        dict[int|None, str]: a dictionary with the list of genres found in the range given
+    """
+    scanned_genres: dict[int|None, str] = {None: "General"}
+    
+    for genre in range(min, max):
+        g = indentify_and_acquire_genre(genre)
+        if g:
+            scanned_genres[genre] = g
+            
+    return scanned_genres
+
+
+def main() -> int:
+    # Try for genre 0 to 99 
+    # Though currently only 11 to 31 seem to be used
+    
+    global genres
+    genres = get_genres(min=8, max=32)
+
+    save_genres()
+        
+    return 0
+
+
+if  __name__ in "__main__":
+    main()
