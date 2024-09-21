e7082917be58714982f1a5a7180e2825c97fbc06
Squizzy <crazy_squirrel@yahoo.com>
2024-09-21 18:34:56 +0100

    Checks if subtitle is empty, replaces with episode number - as suggested by solidsnake1298

--- Python/CreateNHKXMLTV.py
+++ Python/CreateNHKXMLTV.py
@@ -174,7 +174,7 @@ def Generate_xmltv_xml(nhkimported: dict) -> xml.Element:
                                                 'channel':'nhk.world'})
 
         Add_xml_element(programme, 'title', attributes={'lang': 'en'}, text=item["title"])
-        Add_xml_element(programme, 'sub-title', attributes={'lang': 'en'}, text=item["subtitle"])
+        Add_xml_element(programme, 'sub-title', attributes={'lang': 'en'}, text=item["subtitle"] if item["subtitle"] else item["airingID"])
         Add_xml_element(programme, 'desc', attributes={'lang': 'en'}, text=item["description"])
         Add_xml_element(programme, 'episode-num', text=item["airingId"])
         Add_xml_element(programme, 'icon', attributes={'src': URL_OF_NHK_ROOT + item["thumbnail"]})
